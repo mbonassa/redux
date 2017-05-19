f9d1e2aec2624bbb89fdfaf502fa5781d80cc2f5
Eli Nemzer <elinemzer7@gmail.com>
2017-05-19 11:22:54 -0400

    test

--- browser/react/store.js
+++ browser/react/store.js
@@ -1,4 +1,11 @@
 import reducer from './reducers/root-reducer';
 import {createStore} from 'redux';
+import {applyMiddleware} from 'redux'
+import {loggerMiddleware} from 'redux-logger'
 
-export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const result = applyMiddleware(loggerMiddleware)
+
+export default createStore(reducer, result);
+
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
